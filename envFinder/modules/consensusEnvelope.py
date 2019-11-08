4068c8b5ca54e37567b8f5e439a96d4332f16bfe
mauraisa <aaron.maurais@bc.edu>
2019-11-08 14:41:29 -0500

    Updates to ConsensusEnvelope

--- envFinder/modules/consensusEnvelope.py
+++ envFinder/modules/consensusEnvelope.py
@@ -1,8 +1,9 @@
 
 from typing import List
-from sortedcontainers import SortedList
+import sys
+import numpy as np
 
-import utils
+from .utils import find_nearest_index
 
 class Isotope(object):
     __slots__ = ['mz', 'int']
@@ -12,6 +13,7 @@ class Isotope(object):
         self.mz = mz
         self.int = intensity
 
+
 class DataPoint(object):
 
     def __init__(self, mz, intensity):
@@ -20,6 +22,31 @@ class DataPoint(object):
 
     def found(self) -> bool:
         return self.link == None
+
+    def print(self, out = sys.stdout,
+              pretty_print = True, print_link = True):
+        '''
+        Print DataPoint to out.
+
+        :param out: Stream to print to.
+        :param pretty_print: If true pretty formatted text will be printed,
+        if false, easily parsed text is printed.
+        :type pretty_print: bool
+        :param print_link: Should the mz and int of linked point be printed?
+        :type print_link: bool
+        '''
+
+        if pretty_print:
+            out.write('{}, {}'.format(self.point.mz, self.point.int))
+            if print_link:
+                out.write(' -> {}, {}'.format(self.link.point.mz, self.link.point.int))
+            out.write('\n')
+        else:
+            out.write('{}\t{}'.format(self.point.mz, self.point.int))
+            if print_link:
+                out.write('\t{}\t{}'.format(self.link.point.mz, self.link.point.int))
+            out.write('\n')
+
 
 def _getToleranceFunction(toleranceType: str, range: float):
     '''
@@ -52,53 +79,74 @@ class ConsensusEnvelope(object):
     #def _iterate(iterable):
     #    for i in
 
-    def __init__(self, actual: list = None, theoretical = None,
+    def __init__(self, actual: List[DataPoint] = None, theoretical: List[Isotope] = None,
                  tolerance: float = 50, toleranceType:str = 'ppm'):
         self._inRange = _getToleranceFunction(toleranceType, tolerance)
         self._actual : List = actual
         self._theoretical : List = theoretical
-        #self._all_labels : List = list()
         self.initialized = False
 
 
     def setActual(self, actual: list):
         self._clearLinks(self._theoretical)
-        #self._all_labels = None
         self.initialized = False
         self._actual = actual
 
+
     def setTheoretical(self, theoretical: List):
         self._clearLinks(self._actual)
-        #self._all_labels = None
         self.initialized = False
         self._theoretical = theoretical
+
 
     def iterActual(self):
         return enumerate(self._actual)
 
+
     def iterTheoretical(self):
         return enumerate(self._theoretical)
 
-    #def iterLabels(self):
-    #    return enumerate(self._all_labels)
 
-    def annotate(self):
+    def annotate(self, remove_unlabeled: bool = False, normalize = True):
 
         self._clearLinks(self._theoretical)
         self._clearLinks(self._actual)
 
         for i, peak in enumerate(self._theoretical):
-            best_i = utils.find_nearest_index(self._actual, peak,
-                                         arrKey = lambda x: x.point.mz)
+            best_i = find_nearest_index(self._actual, peak,
+                                        valueKey= lambda x: x.point.mz,
+                                        arrKey = lambda x: x.point.mz)
 
-            if self._inRange(self._actual[best_i].point.mz, peak):
+            if self._inRange(self._actual[best_i].point.mz, peak.point.mz):
                 self._actual[best_i].link = peak
-                self._theoretical[i].link = self._actual[best_i].link
+                self._theoretical[i].link = self._actual[best_i]
 
+        if remove_unlabeled:
+            self._actual = [x for x in self._actual if x.link is not None]
+
+        if normalize:
+            try:
+                max_int = max([x.point.int for x in self._actual if x.link is not None])
+            except ValueError:
+                sys.stdout.write('No points in envelope found!\n')
+                max_int = max(self._actual, key = lambda x: x.point.int).point.int
+
+            for i in range(len(self._actual)):
+                self._actual[i].point.int /= max_int
+
+            max_int = max(self._theoretical, key = lambda x: x.point.int).point.int
+            for i in range(len(self._theoretical)):
+                self._theoretical[i].point.int /= max_int
+
+        self.calcEnvScore()
         self.initialized = True
 
+
     def calcEnvScore(self):
-        pass
+        cor = np.corrcoef(x = [x.point.int for x in self._actual],
+                          y = [x.link.point.int if x.link is not None else 0 for x in self._actual])
+        self.envScore = cor[0,1]
+
 
     def plotEnv(self, fname:str):
         pass
